5fa23339f1043da9c8c3f1cbc8fc5d5cfb37f37b
Hannes <hannes.e.falk@outlook.com>
2022-06-16 20:03:03 +0200

    Parent -> Child communicaiton

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,10 @@ export class ProductListComponent {
   share(product: Product) {
     window.alert(`The product has been shared! ${product.name}`);
   }
+
+  onNotify() {
+    window.alert("You will be notified when the product goes on sale");
+  }
 }
 
 /*
